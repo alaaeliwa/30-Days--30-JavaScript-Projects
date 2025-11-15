9c51fd9080bf209fa209ff2970af4344651398be
Alaa' Eliwa <alaahanieliwa@gmail.com>
2025-11-15 09:40:18 +0200

    Project 2 -->finish

--- Day2/script.js
+++ Day2/script.js
@@ -1,19 +1,50 @@
-const apiKey = "863242cfb2b1d357e6093d9a4df194ab";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const addButton = document.querySelector("button");
+const warningOverlay = document.getElementById("warning-overlay");
 
-async function checkWeather(city) {
-  try {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    if (!response.ok) {
-      throw new Error("City not found or API error");
-    }
-    const data = await response.json();
-    console.log(data);
-    document.querySelector(".city").textContent = data.name;
-    document.querySelector(".temp").textContent = Math.round(data.main.temp) + "°C";
-  } catch (error) {
-    console.error("❌ Error fetching weather:", error);
+addButton.onclick = () => {
+  if (inputBox.value.trim() === "") {
+    warningOverlay.classList.add("show"); // نُظهر الرسالة بالنص
+    return;
   }
+
+  let li = document.createElement("li");
+  li.innerHTML = inputBox.value;
+  listContainer.appendChild(li);
+ 
+  let span = document.createElement("span");
+  span.innerHTML = "\u00d7";
+  li.appendChild(span);
+
+  inputBox.value = "";
+  saveData();
+};
+
+warningOverlay.addEventListener("click", () => {
+  warningOverlay.classList.remove("show");
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+// save data
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
 
-checkWeather("bangalore");
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
